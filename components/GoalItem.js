4f76ee2e10b04d776fbbb0cf66ed1f35e5886e38
Yael <maka28684@gmail.com>
2024-05-02 15:12:12 -0700

    three commit

---
+++ components/GoalItem.js
@@ -0,0 +1,27 @@
+import { StyleSheet, View; Text, Pressable } from "react-native";
+
+function GoalItem(itemData, OnDeleteItem){
+    return(
+        <Pressable>
+            <View style={style.goalsItem}>
+                <Text style={style.goalText}>{itemData.item}</Text>
+
+            </View>
+        </Pressable>
+    )
+}
+
+export default GoalItem
+
+const styles = StyleSheet.create({
+    goalsItem: {
+        margin: 8,
+        padding: 8,
+        borderRadius: 6,
+        backgroundColor: "#8576FF",
+        color: "white"
+    },
+    goalText: {
+        color: "white"
+    }
+})
